ec0d11163b758caa949fab19d3a1d62c64a0278f
af11_sanger <af11@sanger.ac.uk>
2014-01-16 13:56:42 +0000

    removed crispr_pairs from REST API

--- lib/WGE/Controller/REST/Crispr.pm
+++ lib/WGE/Controller/REST/Crispr.pm
@@ -1,6 +1,7 @@
 package WGE::Controller::REST::Crispr;
 
 use Moose;
+use Try::Tiny;
 use namespace::autoclean;
 use Data::Dumper;
 
@@ -22,24 +23,16 @@ sub crispr_POST{
     my ($self, $c) = @_;
 
     $c->assert_user_roles('edit');
+    my @update_cols = qw(off_targets off_target_summary);
 
-}
+    my $params = $c->req->params;
 
-sub crispr_pair : Path( '/api/crispr_pair') : Args(0) :ActionClass( 'REST' ) {}
+    my $crispr = $c->model->resultset('Crispr')->find($params->{id});
+    my %update_params = map { $_ => $params->{$_} } grep { exists $params->{$_} } @update_cols;
 
-sub crispr_pair_GET{
-    my ($self, $c) = @_;
+    $crispr->update(\%update_params) or die $!;
 
-    $c->assert_user_roles('read');
-
-    my $crispr_pair = $c->model('DB')->resultset('CrisprPair')->find($c->req->param('id'));
-    return $self->status_ok( $c, entity => $crispr_pair->as_hash );
-}
-
-sub crispr_pair_POST{
-    my ($self, $c) = @_;
-
-    $c->assert_user_roles('edit');
+    return $self->status_ok( $c, entity => $crispr->as_hash );
 }
 
 1;
