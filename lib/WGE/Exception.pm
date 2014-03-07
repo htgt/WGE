8e50c08e9c0c8870cb70f366aefc9fb11308e138
Sajith Perera <sajith.perera@gmail.com>
2014-03-07 14:12:46 +0000

    add WGE Exception classes

---
+++ lib/WGE/Exception.pm
@@ -0,0 +1,32 @@
+package WGE::Exception;
+use warnings FATAL => 'all';
+
+use Moose;
+use MooseX::ClassAttribute;
+use namespace::autoclean;
+
+class_has show_stack_trace => (
+    is      => 'rw',
+    isa     => 'Bool',
+    default => 0
+);
+
+extends qw( Throwable::Error );
+
+override as_string => sub {
+    my ( $self ) = @_;
+
+    my $str = $self->message;
+
+    if ( $self->show_stack_trace ) {
+        $str .= "\n\n" . $self->stack_trace->as_string;
+    }
+
+    return $str;
+};
+
+__PACKAGE__->meta->make_immutable( inline_constructor => 0 );
+
+1;
+
+__END__
