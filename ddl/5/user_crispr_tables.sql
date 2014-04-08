7408e41f0b4898020ee6329cda6f7811a61556bf
af11_sanger <af11@sanger.ac.uk>
2014-04-08 17:15:48 +0100

    tables to link user to crisprs and crispr pairs

---
+++ ddl/5/user_crispr_tables.sql
@@ -0,0 +1,23 @@
+CREATE TABLE user_crisprs_human (
+       crispr_id  INTEGER NOT NULL REFERENCES crisprs_human(id),
+       user_id    INTEGER NOT NULL REFERENCES users(id),
+       PRIMARY KEY(crispr_id, user_id)
+);
+
+CREATE TABLE user_crispr_pairs_human (
+       crispr_pair_id  TEXT NOT NULL REFERENCES crispr_pairs_human(id),
+       user_id    INTEGER NOT NULL REFERENCES users(id),
+       PRIMARY KEY(crispr_pair_id, user_id)
+);
+
+CREATE TABLE user_crisprs_mouse (
+       crispr_id  INTEGER NOT NULL REFERENCES crisprs_mouse(id),
+       user_id    INTEGER NOT NULL REFERENCES users(id),
+       PRIMARY KEY(crispr_id, user_id)
+);
+
+CREATE TABLE user_crispr_pairs_mouse (
+       crispr_pair_id  TEXT NOT NULL REFERENCES crispr_pairs_mouse(id),
+       user_id    INTEGER NOT NULL REFERENCES users(id),
+       PRIMARY KEY(crispr_pair_id, user_id)
+);
