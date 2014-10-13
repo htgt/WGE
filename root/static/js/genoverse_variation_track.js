bf12517bf6a6b5da5935789c12c79c1b5e8e09e9
David Parry-Smith <david.parry-smith@sanger.ac.uk>
2014-10-13 14:32:28 +0100

    First pass at variation track

---
+++ root/static/js/genoverse_variation_track.js
@@ -0,0 +1,7 @@
+Genoverse.Track.Variation = Genoverse.Track.extend({
+        // The basics for a variation track reading from Ensembl
+    test: function f() {
+        console.log('Variation track setup');
+        }
+});
+
