02f46d23bb26f613d9cf7563ae2d5e7ae228d15f
Alex Hodgkins <ah19@sanger.ac.uk>
2014-11-07 16:25:17 +0000

    Bugfix: individual off target API method did not work

--- lib/WGE/Controller/API.pm
+++ lib/WGE/Controller/API.pm
@@ -275,13 +275,22 @@ sub individual_off_target_search :Local('individual_off_target_search') {
     my ( $self, $c ) = @_;
 
     my $params = $c->req->params;
-    check_params_exist( $c, $params, [ qw( species ids ) ] );
+    check_params_exist( $c, $params, [ qw( species ids[] ) ] );
 
+    try {
+        my $data = $self->ot_finder->run_individual_off_target_search(
+            $c->model('DB'),
+            $params->{species},
+            $params->{'ids[]'}
+        );
 
+        $c->stash->{json_data} = $data // {};
+    }
+    catch ( $e ) {
+        $e = $e->as_string if ref $e;
+        $c->stash->{json_data} = { error => $e };
+    }
 
-    my $data = $self->ot_finder->run_individual_off_target_search( $c->model('DB'), $params );
-
-    $c->stash->{json_data} = $data;
     $c->forward('View::JSON');
 
     return;
@@ -294,7 +303,7 @@ sub pair_off_target_search :Local('pair_off_target_search') {
 
     check_params_exist( $c, $params, [ qw( species left_id right_id ) ] );
 
-    my $data = $self->ot_finder->run_pair_off_target_search($c->model('DB'),$params);
+    my $data = $self->ot_finder->run_pair_off_target_search($c->model('DB'), $params);
 
     $c->stash->{json_data} = $data;
     $c->forward('View::JSON');
