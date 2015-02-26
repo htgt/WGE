d4c8494d19be13bd42abcabc1e2ff3e8c8b93637
Sajith Perera <sajith.perera@gmail.com>
2015-02-26 09:20:34 +0000

    add method to calculate mismatch score for a off target

--- lib/WGE/Model/Schema/Result/CrisprOffTargets.pm
+++ lib/WGE/Model/Schema/Result/CrisprOffTargets.pm
@@ -30,10 +30,10 @@ __PACKAGE__->result_source_instance->is_virtual(1);
 __PACKAGE__->result_source_instance->view_definition( <<'EOT' );
 with ots as (
     select x.crispr_id, unnest(off_target_ids) as ot_id
-    from (SELECT unnest(?::int[]) as crispr_id) x 
+    from (SELECT unnest(?::int[]) as crispr_id) x
     join crisprs on crisprs.id=x.crispr_id and crisprs.species_id=?
 )
-select  
+select
     ots.crispr_id as parent_id,
     c.*
 from ots
@@ -47,7 +47,7 @@ __PACKAGE__->add_columns(
         parent_id
         chr_name
         chr_start
-        seq 
+        seq
         pam_right
         species_id
         off_target_ids
@@ -84,6 +84,33 @@ sub as_hash {
     }
 
     return $data;
+}
+
+sub mismatches {
+    my ( $self, $crispr_grna ) = @_;
+
+    unless ( $crispr_grna ) {
+        my $crispr = $self->result_source->schema->resultset('Crispr')->find( { id => $self->parent_id } );
+        my $crispr_seq = $crispr->pam_right ? $crispr->seq : revcom_as_string( $crispr->seq );
+        $crispr_grna = substr $crispr_seq, 0, 20;
+    }
+
+    my $ot_seq = $self->pam_right ? $self->seq : revcom_as_string( $self->seq );
+    my $ot_grna = substr $ot_seq, 0, 20;
+
+    return hamming_distance( $crispr_grna, $ot_grna );
+}
+
+=head2 hamming_distance
+
+use string xor to get the number of mismatches between the two strings.
+the xor returns a string with the binary digits of each char xor'd,
+which will be an ascii char between 001 and 255. tr returns the number of characters replaced.
+
+=cut
+sub hamming_distance {
+    die "Strings passed to hamming distance differ" if length($_[0]) != length($_[1]);
+    return (uc($_[0]) ^ uc($_[1])) =~ tr/\001-\255//;
 }
 
 __PACKAGE__->set_primary_key( "id" );
