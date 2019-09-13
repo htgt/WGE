5db86984620d8fe41dcd78e086e03f518de717b8
jmrein <jr27@sanger.ac.uk>
2019-09-13 15:01:12 +0100

    Specifically include license information

--- bin/import_vcf.pl
+++ bin/import_vcf.pl
@@ -1,4 +1,7 @@
 #!/usr/bin/perl
+# Copyright (C) 2019 Genome Research Limited
+# This program is licensed under the GNU Affero General Public License, v3 or later.
+# Further details can be found by running with --help, or from the included License file.
 use strict;
 use warnings;
 use Carp;
@@ -98,4 +101,22 @@ The VCF file to read.
 =head1 DESCRIPTION
 
 This program reads a VCF and exports the specific data needed to create a haplotype track (as a CSV which can be \copy-ed in).
+
+=head1 LICENSE AND COPYRIGHT
+
+import_vcf.pl
+Copyright (C) 2019 Genome Research Limited
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
